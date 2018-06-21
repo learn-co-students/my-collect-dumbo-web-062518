bdafd5c2ddd4944213444a11b014813dc657f1c5
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 19:39:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(collection)
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < collection.length
+      variable = yield collection[i]
+      new_collection << variable
+      i = i + 1
+    end
+    new_collection
+  else
+    return nil
+  end
+end
